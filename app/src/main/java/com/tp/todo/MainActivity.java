53f1136d1a6ded5452d9510c05f09c66f4c53a04
Thomos69 <thomasgaujard@gmail.com>
2020-12-09 21:26:45 +0100

    Ajout de la liste déroulante + petit changement visuels

--- app/src/main/java/com/tp/todo/MainActivity.java
+++ app/src/main/java/com/tp/todo/MainActivity.java
@@ -2,6 +2,9 @@ package com.tp.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_add_todo);
+
+        Spinner list = findViewById(R.id.list);
+
+        // Permet de remplir la liste déroulante
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.importance));
+        listAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        list.setAdapter(listAdapter);
     }
 }
