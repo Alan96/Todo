0b8dfa5eaf3482f88d33113a66c50004b7c33879
Alan96 <60261542+Alan96@users.noreply.github.com>
2020-12-09 18:22:00 +0100

    Todov2

---
+++ app/src/main/java/com/tp/todo/AddTodoActivity.java
@@ -0,0 +1,13 @@
+package com.tp.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_todo);
+    }
+}
